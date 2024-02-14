d7e94a2353e9510582c2345ed08bc0a330e97b83
FortyKin <dikow0605@gmail.com>
2024-02-14 19:17:38 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './component/Login';
+import Register from './component/Register';
 
-function App() {
+const App: React.FC = () => {
+  const [isLogin, setIsLogin2] = useState(true);
+
+  const handleSwitchMode = () => {
+    setIsLogin2(!isLogin);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLogin ? (
+        <Login RegisterClick={handleSwitchMode} />
+      ) : (
+        <Register LoginClick={handleSwitchMode} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
